00c9e4461ed04bb86af5905ced59ece2d03d7e89
unknown <b3esho2012@gmail.com>
2020-10-15 20:40:37 +0300

    Updates

---
+++ precache-manifest.d55ab8d90fc199d515b00a9413b91974.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc504cff79f4a802036049d92d3d1ed4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "36a8dbac43333c408f81",
+    "url": "/monsters-rolodex/static/css/main.939c7f96.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36a8dbac43333c408f81",
+    "url": "/monsters-rolodex/static/js/main.b5d0c45a.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
